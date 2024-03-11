a7da89f7f024271d83f0dbb390fa81b289635222
Usha Kumari <uavaswani@gmail.com>
2024-03-11 23:25:29 +0100

    Add frontend files

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from './Components/Home';
 import NavBar from './Components/Navbar';
 import UserRegister from './Components/UserRegister';
 import SignUp from "./Components/SignIn";
-import Login from './Components/LogIn'; // Corrected import statement
+import Login from './Components/LogIn'; 
 //import Confirmation from  './Components/Confirmation';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
