c29a6dbe9f298c4119fe87c5e79be4e4fe858c40
Usha Kumari <uavaswani@gmail.com>
2024-03-11 22:59:48 +0100

    Add backend files

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser'); // Import body-parser module
+
+// Parse JSON request bodies
+app.use(express.json());
+
+// Import the function for MongoDB connection from database.js
+const { mongoDbConnection } = require('./database');
+
+// Call the mongoDbConnection function to establish the connection
+mongoDbConnection();
+
+// Add your CORS middleware
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    // Other CORS headers if needed
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    // Allow cookies to be sent from the client
+    res.setHeader('Access-Control-Allow-Credentials', true);
+  
+    next();
+  });
+
+// Define your routes and middleware
+app.use('/api', require('./Routers/createUser'));
+
+// Start the Server
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
