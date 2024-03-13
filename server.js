a68486c2dd2d7eca7754e956dfd98164750fa42b
Usha Kumari <uavaswani@gmail.com>
2024-03-13 10:41:55 +0100

    deployed url

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ mongoDbConnection();
 
 // Add your CORS middleware
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'https://user-registeration-git-main-ushas-projects-d69436a1.vercel.app');
     // Other CORS headers if needed
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
