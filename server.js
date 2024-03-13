110af206c74101c94a89f573c825e90ffc300445
Usha Kumari <uavaswani@gmail.com>
2024-03-13 21:23:15 +0100

    products database

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const bodyParser = require('body-parser'); // Import body-parser module
 
+const productRouter = require('./Routers/createProduct');
 // Parse JSON request bodies
 app.use(express.json());
+
+// Add your CORS middleware
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  // Other CORS headers if needed
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  // Allow cookies to be sent from the client
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  next();
+});
 
 // Import the function for MongoDB connection from database.js
 const { mongoDbConnection } = require('./database');
@@ -12,20 +24,9 @@ const { mongoDbConnection } = require('./database');
 // Call the mongoDbConnection function to establish the connection
 mongoDbConnection();
 
-// Add your CORS middleware
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://user-registeration-git-main-ushas-projects-d69436a1.vercel.app');
-    // Other CORS headers if needed
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    // Allow cookies to be sent from the client
-    res.setHeader('Access-Control-Allow-Credentials', true);
-  
-    next();
-  });
-
 // Define your routes and middleware
 app.use('/api', require('./Routers/createUser'));
+app.use('/api/products', productRouter);
 
 // Start the Server
 const PORT = process.env.PORT || 8000;
